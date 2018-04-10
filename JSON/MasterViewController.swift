88feb6fb74af54248b44c8402c6b581925e9bdcc
Dakota Mathews <djm12b@mbb-302-mac-02.local>
2018-04-10 13:20:42 -0500

    I hope this works and yes

--- JSON/MasterViewController.swift
+++ JSON/MasterViewController.swift
@@ -13,14 +13,17 @@ class MasterViewController: UITableViewController {
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
 
+    let URL_DET = "https://api.myjson.com/bins/i53ev"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
+        getJsonFromUrl();
+        
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+//        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -36,20 +39,61 @@ class MasterViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+    
+    //this function is fetching the json from URL
+    func getJsonFromUrl(){
+        //creating a NSURL
+        let url = NSURL(string: URL_DET)
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: (url! as URL), completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                
+                //printing the json in console
+                print(jsonObj!.value(forKey: "stuff")!)
+                self.objects = jsonObj!.value(forKey: "stuff") as! [Any]
+                
+                //getting the avengers tag array from json and converting it to NSArray
+//                if let DETArray = jsonObj!.value(forKey: "stuff") as? NSArray {
+//                    //looping through all the elements
+//                    for DET in DETArray{
+//
+//                        //converting the element to a dictionary
+//                        if let DETDict = DET as? NSDictionary {
+//
+//                            //getting the name from the dictionary
+//                            if let name = DETDict.value(forKey: "name") {
+//
+//                                //adding the name to the array
+//                                self.nameArray.append((name as object)!)
+//                            }
+//
+//                        }
+//                    }
+//                }
+                
+                OperationQueue.main.addOperation({
+                    //calling another function after fetching the json
+                    //it will show the names to label
+                    //self.showNames()
+                })
+            }
+        }).resume()
     }
+//    @objc
+//    func insertNewObject(_ sender: Any) {
+//        objects.insert(NSDate(), at: 0)
+//        let indexPath = IndexPath(row: 0, section: 0)
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+//    }
 
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = objects[indexPath.row] as! String
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
@@ -71,7 +115,7 @@ class MasterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
+        let object = objects[indexPath.row] as! String
         cell.textLabel!.text = object.description
         return cell
     }
