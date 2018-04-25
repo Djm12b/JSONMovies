dba552707766e6f0153bc93ec5948a1de0e6c181
Dakota Mathews <djm12b@mbb-302-mac-01.local>
2018-04-25 02:26:43 -0500

    try following a youtube video and did not work out well

--- JSON/MasterViewController.swift
+++ JSON/MasterViewController.swift
@@ -12,12 +12,17 @@ class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
 
-// the json Url
-    let URL_IMDB = "https://api.myjson.com/bins/1ahrbf"
+    struct Movie: Decodable{
+        let franchiseName: String
+        let name: String
+        
+        //OLD
+//        init(json: [String: Any]) {
+//            franchiseName = json["franchiseName"] as? String ?? ""
+//            name = json["name"] as? String ?? ""
+//        }
+    }
     
-    var nameArray = [String]()
-    var objects = [Any]()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,121 +31,44 @@ class MasterViewController: UITableViewController {
         self.navigationController?.navigationBar.barTintColor = UIColor.darkGray
         // Do any additional setup after loading the view, typically from a nib.
         // navigationItem.leftBarButtonItem = editButtonItem
-        
-                  getJsonFromUrl();
-
-//       let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-       //navigationItem.rightBarButtonItem = addButton
+    
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
-    }
 
-    override func viewWillAppear(_ animated: Bool) {
+        func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    //this function is fetching the json from URL
-    func getJsonFromUrl(){
-        //creating a NSURL
-        let url = NSURL(string: URL_IMDB)
+        }
+        let jsonUrlString = "https://api.myjson.com/bins/1ahrbf"
+        guard let url = URL(string: jsonUrlString)
+            else {return}
         
-        //fetching the data from the url
-        URLSession.shared.dataTask(with: (url! as URL), completionHandler: {(data, response, error) -> Void in
+        URLSession.shared.dataTask(with: url) {( data, responds, err) in
+        //perhaps check err
+        //also perhaps check response status 200 OK
+        
+        guard let data = data else { return }
+        
+//        let dataAsString = String(data: data, encoding: .utf8)
+//    print(dataAsString)
+        
+            do{
+                let movies = try JSONDecoder().decode([Movie].self, from: data)
+                    print(movies)
+                //swift 2/3/odjC OLD!!!
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {return}
+//
+//
+//                let course = Course(json: json)
+//                print(course.name)
+           }
+            catch let jsonErr {
+                print("Error Serializing JSON:", jsonErr )
+            }
             
-            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
-                
-                //printing the json in console
-                print(jsonObj!.value(forKey: "franchise")!)
-                self.nameArray = jsonObj!.value(forKey: "franchise") as! [Any] as! [String]
-                
-                //getting the avengers tag array from json and converting it to NSArray
-                if let MoviesArray = jsonObj!.value(forKey: "franchiseName") as? NSArray {
-                    //looping through all the elements
-                    for Movies in MoviesArray{
-
-                        //converting the element to a dictionary
-                        if let MoviesDict = Movies as? NSDictionary {
-
-                            //getting the name from the dictionary
-                            if let name = MoviesDict.value(forKey: "name") {
-
-                                //adding the name to the array
-                                self.nameArray.append((name as? String)!)
-                            }
-
-                        }
-                    }
-                }
-                
-                OperationQueue.main.addOperation({
-                    //calling another function after fetching the json
-                    //it will show the names to label
-                    //self.showNames()
-                })
-            }
-        }).resume()
+            
+            }.resume()
     }
-//    @objc
-//    func insertNewObject(_ sender: Any) {
-//        objects.insert(NSDate(), at: 0)
-//        let indexPath = IndexPath(row: 0, section: 0)
-//        tableView.insertRows(at: [indexPath], with: .automatic)
-//    }
-
-    // MARK: - Segues
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! String
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
-    }
-
-    // MARK: - Table View
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return objects.count
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! String
-        cell.textLabel!.text = object.description
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return false
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
-
 }
-
